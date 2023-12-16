6dcd614f12502582c5fb1ae156dfeeaf78bf197e
rodrimarti1 <alvarojeronimo93@gmail.com>
2023-12-15 23:14:17 -0600

    add last java class to location functionalities

---
+++ src/test/java/steps/ITO/LocationTextHeader.java
@@ -0,0 +1,16 @@
+package steps.ITO;
+
+import com.thoughtworks.gauge.Step;
+import object.ItoPageObject;
+import org.assertj.core.api.Assertions;
+
+
+public class LocationTextHeader {
+    ItoPageObject itoPageObject = new ItoPageObject();
+    @Step("Then validate the <inputLocationText> title location is displayed")
+    public void thenValidateTheInputLocationTextTitleLocationIsDisplayed(String inputLocationText) throws InterruptedException {
+        Assertions.assertThat(itoPageObject.cityCatalog().toString())
+                .withFailMessage("The "+ inputLocationText +" title don't display")
+                .containsIgnoringCase(inputLocationText);
+    }
+}
